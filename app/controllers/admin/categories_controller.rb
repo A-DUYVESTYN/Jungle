db978efc1b05449815de4578694057cce97d7b52
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-08-29 19:04:58 +0000

    set up RSpec testing

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV["ADMIN_PASSWORD"]
+  
   def index
     @categories = Category.all.order(id: :desc)
   end
