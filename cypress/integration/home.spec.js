66ec7f63582d662f890faf4525c8295326df35ca
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-08-30 19:46:54 +0000

    add cypress homepage test

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+describe('Jungle app', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('/')
+  })
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
